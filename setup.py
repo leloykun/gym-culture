af4e9ec8d4627a12f619b94d1de4e0256ce8cb22
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-03-11 01:28:53 +0800

    added more info in setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup
 
 setup(name='gym_culture',
       version='0.0.2.1',
+      url='https://github.com/LE-LOY/gym-culture',
       author='Franz Louis Cesista',
+      license='MIT',
       packages=['gym_culture', 'gym_culture.envs'],
       package_data = {
           "gym_culture.envs": ["assets/*.txt", "widgets/*.py"]
       },
-      install_requires=['gym']  # dependencies
+      install_requires=['gym', 'pygame', 'numpy', 'matplotlib']  # dependencies
 )

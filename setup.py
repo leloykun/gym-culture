45924a6e1412392f3176701d00f2df6491e17af8
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-03-11 00:33:12 +0800

    version 0.0.10: addition of display

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='gym_culture',
-      version='0.0.9',
+      version='0.0.10',
       author='Franz Louis Cesista',
       packages=['gym_culture', 'gym_culture.envs'],
       package_data = {
-          "gym_culture.envs": ["assets/*.txt"]
+          "gym_culture.envs": ["assets/*.txt", "widgets/*py"]
       },
       install_requires=['gym']  # dependencies
 )

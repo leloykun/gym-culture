23a553f0d12632c552db7aecf415430f449ce83a
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-03-11 00:40:28 +0800

    minor bug fix

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='gym_culture',
-      version='0.0.10',
+      version='0.0.11',
       author='Franz Louis Cesista',
       packages=['gym_culture', 'gym_culture.envs'],
       package_data = {
-          "gym_culture.envs": ["assets/*.txt", "widgets/*py"]
+          "gym_culture.envs": ["assets/*.txt", "widgets/*.py"]
       },
       install_requires=['gym']  # dependencies
 )
